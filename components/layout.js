629c165bcd0c1c30d07d320c80d7cafe9ebddba0
yohei <si18024@edu.aomori-u.ac.jp>
2021-09-15 15:54:57 +0900

    レイアウトcssの追加とページ変更

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,6 @@
-function Layout({ children }) {
-  return <div>{children}</div>
+import styles from './layout.module.css'
+
+export default function Layout({ children }) {
+  return <div className={styles.container}>{children}</div>
 }
 
-export default Layout
